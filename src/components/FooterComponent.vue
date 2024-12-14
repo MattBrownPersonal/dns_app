79f730affb629b4ca1acb3424b4dc68f0bb0ae41
Matt Brown <tonganninja@googlemail.com>
2024-12-14 16:30:24 +0000

    refactored into components

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,84 @@
+<template>
+
+    <div class="flex flex-col items-center justify-center">
+        <div class="grid grid-cols-2 gap-4 w-full contact_us_container">
+            <div class="flex flex-col py-12">
+                <div class="contact_us_text">
+                    <p>We're <span class="text-outline">your</span> IT Services</p>
+                    <p>problem solvers</p>
+                </div>
+                <div class="large_ibm_font pt-5">
+                    Recognising your frustrations with your print environment, IT services, document management &
+                    communications
+                    and finding.
+                    <br><br> Get in touch today!
+                </div>
+            </div>
+            <form action="">
+                <div class="flex flex-col py-20">
+                    <!-- TODO: impliment form backend api -->
+                    <input type="text" class="rounded text-white input_box placeholder-white mb-6" placeholder="Name" />
+                    <input type="text" class="rounded text-white input_box placeholder-white mb-6"
+                        placeholder="Email" />
+                    <input type="text" class="rounded text-white input_box placeholder-white mb-6"
+                        placeholder="Telephone" />
+                    <input type="text" class="rounded text-white input_box placeholder-white mb-6"
+                        placeholder="Company" />
+                    <input type="text" class="rounded text-white input_box placeholder-white mb-6"
+                        placeholder="I need help with..." />
+                    <div class="flex justify-end">
+                        <button type="submit" class="bg-white text-black px-6 py-2 submit_button large_ibm_font">Submit
+                            Now</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <img src="/assets/geometric_pink.png" alt="" class="geometric_pink">
+    <div class="flex flex-col items-center justify-center text-white">
+        <div class="grid grid-cols-6 gap-4 w-full footer">
+            <div><img src="/assets/dns_logo.png" alt="DNS Logo"></div>
+            <div>
+                <p>Contact us</p>
+                <p>E infor@dnslimited.co.uk</p>
+                <p>T 0845 034 0895</p>
+            </div>
+            <div>
+                <p>Address</p>
+                <p>Unit 9</p>
+                <p>Royal Scot Road</p>
+                <p>Pride Park, Derby</p>
+                <p>DE24 8AJ</p>
+            </div>
+            <div>
+                <p>Opening Hours</p>
+                <p>Min - Fri: </p>
+                <p>9 am - 5:30 pm</p>
+            </div>
+            <div>
+                <p>About Us</p>
+                <p>CSR</p>
+                <p>Environment & Sustainability</p>
+                <p>Meet the team</p>
+            </div>
+            <div>
+                <p>Services </p>
+                <p>IT Services</p>
+                <p>Communications </p>
+                <p>Managed Print Services </p>
+                <p>Document Management</p>
+            </div>
+        </div>
+    </div>
+    <hr class="text-white mt-12">
+    <div class="flex justify-between text-white pt-6 footer_copywright px-8">
+        <div>Design & built by Alt</div>
+        <div>&copy; Document Network Services Ltd 2024</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
